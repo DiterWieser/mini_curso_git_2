2f5405210d0cae2303e1b1e1a1c64f10cf7fcd80
Diter M Wieser <dmwieser@yahoo.com.br>
2017-10-05 21:07:25 -0300

    Hello World recursivo

--- CAppGit/main.cpp
+++ CAppGit/main.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
+void print(int valor);
+
 int main() {
-    
-    cout << "Hello World!!!" << endl;
-    
-    return0;
+
+    print(0);
+
+    return 0;
+}
+
+void print(int valor) {
+    cout << valor++ << endl;
+    print(valor);
 }
